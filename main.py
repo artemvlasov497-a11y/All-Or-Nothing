e45202e846fb37475d10ff8c396fad40d3a31473
ArtemVeryCool <artemvlasov497@gmail.com>
2026-04-24 22:37:51 +0500

    Выход игры(1.0)

---
+++ main.py
@@ -0,0 +1,30 @@
+from random import randint
+
+cur_chance = 1
+score = 0
+record = 0
+
+while True:
+    c = int(input('Привет! Играть(1) или узнать рекорд(2)?'))
+    if c == 1:
+        while True:
+            c1 = int(input('Идти дальше(1) или остановиться(2)?'))
+            if c1 == 1:
+                if randint(0, 100) > cur_chance:
+                    score += 1
+                    if cur_chance < 50:
+                        cur_chance += 1
+                    print('Ты выиграл с шансом', 100 - cur_chance, 'и идёшь дальше!')
+                else:
+                    print('Ты проиграл с результатом', str(score) + '. Шанс проиграть был', cur_chance)
+                    score = 0
+                    cur_chance = 0
+                    break
+            if c1 == 2:
+                print('Ты вышел с результатом', score)
+                if record < score:
+                    record = score
+                score = 0
+                break
+    if c == 2:
+        print('Текущий рекорд:', record)
